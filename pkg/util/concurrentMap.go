d8141c94f0729cf21ce484360b283ade72b566d1
Doug Steigerwald <steigerwald.doug@gmail.com>
2019-11-26 12:54:03 -0500

    (GOSVC-665) ConcurrentMap is now using a RWMutex so we can have concurrent reads when there is no write lock.
    Change-Id: Icbb09fb44acd39bf8e649e7c94d7d7b97336b00d

--- pkg/util/concurrentMap.go
+++ pkg/util/concurrentMap.go
@@ -4,48 +4,55 @@ import (
 	"sync"
 )
 
+// ConcurrentMap A map[string]interface{} with a mutex to prevent concurrent access errors
 type ConcurrentMap struct {
-	sync.Mutex
+	sync.RWMutex
 	items map[string]interface{}
 }
 
+// NewConcurrentMap Creates a new ConcurrentMap
 func NewConcurrentMap() *ConcurrentMap {
 	return &ConcurrentMap{
 		items: make(map[string]interface{}),
 	}
 }
 
+// Add Add a key/value to the map
 func (cm *ConcurrentMap) Add(key string, item interface{}) {
 	cm.Lock()
 	defer cm.Unlock()
 	cm.items[key] = item
 }
 
+// Delete Delete a key from the map
 func (cm *ConcurrentMap) Delete(key string) {
 	cm.Lock()
 	defer cm.Unlock()
 	delete(cm.items, key)
 }
 
+// Get Return the value for a given key in the map
 func (cm *ConcurrentMap) Get(key string) (interface{}, bool) {
-	cm.Lock()
-	defer cm.Unlock()
+	cm.RLock()
+	defer cm.RUnlock()
 	if val, ok := cm.items[key]; ok {
 		return val, true
 	}
 	return nil, false
 }
 
+// GetList Return a list of all keys in the map
 func (cm *ConcurrentMap) GetList() []string {
-	cm.Lock()
-	defer cm.Unlock()
+	cm.RLock()
+	defer cm.RUnlock()
 	var items []string
-	for k, _ := range cm.items {
+	for k := range cm.items {
 		items = append(items, k)
 	}
 	return items
 }
 
+// Length Return the length of the map
 func (cm *ConcurrentMap) Length() int {
 	return len(cm.items)
 }
